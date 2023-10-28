a76bad9ad99a7ffdd1679ae15ac90f95e1656d56
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2023-10-28 18:45:15 -0500

    Added highlights section

--- Intermediate_projects/venue/src/App.js
+++ Intermediate_projects/venue/src/App.js
@@ -3,6 +3,9 @@ import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
 import Featured from './components/featured/Featured';
 
+import VenueInfo from './components/venue_info/venueInfo';
+import Highlights from './components/highlights/Highlights';
+
 const App = () => {
   return (
     <div className="App">
@@ -11,6 +14,16 @@ const App = () => {
       <>
         <Featured/>
       </>
+
+      <>
+        <VenueInfo/>
+      </>
+
+      <>
+        <Highlights/>
+      </>
+
+      
       <div style={{backgroundColor: 'red', height: '800px'}} ></div>
       <div style={{backgroundColor: 'blue', height: '800px'}} ></div>
       <div style={{backgroundColor: 'green', height: '800px'}} ></div>
---
+++ Intermediate_projects/venue/src/components/highlights/Discount.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { Fade, Slide } from 'react-awesome-reveal';
+
+const Discount = () => {
+
+    const end = 30;
+
+    const [start, setStart] = useState(0);
+
+    const percentage = () => {
+        if(start < end){
+            setStart(prevCount => prevCount + 1)
+        }
+    }
+
+    useEffect(() => {
+        if(start > 0 && start < 30){
+            setTimeout(() => {
+                setStart(prevCount => prevCount + 1)
+            }, 30)
+        }
+    }, [start])
+
+    return(
+        <div className="center_wrapper">
+            <div className="discount_wrapper">
+                <Fade
+                    onVisibilityChange={(inView) => {
+                        if(inView){
+                            percentage()
+                        }
+                    }}
+                >
+                    <div className="discount_percentage">
+                        <span>{start}%</span>
+                        <span>OFF</span>
+                    </div>
+                </Fade>
+                <Slide right={true}>
+                    <div className="discount_description">
+                        <h3>Purchase tickets before 1st December</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Risus viverra adipiscing at in. Elementum eu 
+                            facilisis sed odio morbi quis commodo odio aenean. Non quam lacus suspendisse faucibus 
+                            interdum posuere lorem. Consequat id porta nibh venenatis cras. Accumsan lacus vel facilisis 
+                            volutpat. Tristique magna sit amet purus gravida quis blandit.
+                        </p>
+                    </div>
+                </Slide>
+            </div>
+        </div>
+    )
+}
+
+export default Discount;
