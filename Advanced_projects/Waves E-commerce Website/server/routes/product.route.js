5320c3ca5eaa7c1f1e942fdbf0c334fbe182b28d
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-03-03 16:42:07 -0600

    CRUD for Products

--- Advanced_projects/Waves E-commerce Website/server/routes/product.route.js
+++ Advanced_projects/Waves E-commerce Website/server/routes/product.route.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const productsController = require('../controllers/products.controller');
+const {addProductValidator} = require('../middleware/validations');
 
-router.post('/', auth('createAny', 'product'), productsController.addProduct )
+router.post('/', auth('createAny', 'product'), addProductValidator, productsController.addProduct )
+// Whatever data we post its gonna go to '/' route and from there to auth() and from there to
+// addProductValidator and from there to controller
 
+router.route('/product/:id')
+.get(productsController.getProductById)
+.patch(auth('updateAny','product'),productsController.updateProductById)
+.delete(auth('deleteAny','product'),productsController.deleteProductById)
+
+router.get('/all', productsController.allProducts);
 
 module.exports = router;
--- Advanced_projects/Waves E-commerce Website/server/services/products.service.js
+++ Advanced_projects/Waves E-commerce Website/server/services/products.service.js
@@ -14,6 +14,61 @@ const addProduct = async(body) => {
     }
 }
 
+const getProductById = async(_id) => {
+    try{
+        const product = await Product.findById(_id).populate('brand') 
+        if(!product) throw new ApiError(httpStatus.NOT_FOUND,'Product not found');
+        return product;
+    } catch(error){
+        throw error;
+    }
+}
+
+const updateProductById = async(id, body) => {
+    try{
+        const product = await Product.findOneAndUpdate(
+            {_id},
+            { "$set": body },
+            { new: true } 
+        );
+        if(!product) throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
+        return product
+    } catch(error){
+        throw error;
+    }
+}
+
+const deleteProductById = async(body) => {
+    try{
+        const product = await Product.findByIdAndRemove(_id);
+        if(!product) throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
+        return product
+    } catch(error){
+        throw error;
+    }
+}
+
+const allProducts = async(req) => {
+    try {
+        const products = await Product
+        .find({})
+        .populate('brand')
+        .sort([
+            [req.query.sortBy,req.query.order]
+        ])
+        .limit(parseInt(req.query.limit));
+
+        return products
+    } catch(error) {
+        throw error
+    }
+}
+
+
 module.exports = {
-    addProduct
+    addProduct,
+    getProductById,
+    updateProductById,
+    deleteProductById,
+    allProducts
 }
